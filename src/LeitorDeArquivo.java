c76df7365e44749b6414b99c6b86c549070c1ed6
JoaoVictorBalvedi <joaovictorbalvedi@gmail.com>
2024-11-07 00:58:24 -0300

    calendario

--- src/LeitorDeArquivo.java
+++ src/LeitorDeArquivo.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 class LeitorDeArquivo {
-    //metodo 5
     public static List<Usuario> carregarUsuarios(String caminhoArquivo) throws TarefaException {
         List<Usuario> usuarios = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
@@ -20,23 +19,5 @@ class LeitorDeArquivo {
             throw new TarefaException("Erro ao ler o arquivo: " + e.getMessage());
         }
         return usuarios;
-    }
-
-    //metodo 6
-    public static boolean verificarUsuarioExistente(String caminhoArquivo, String nomeUsuario) throws TarefaException {
-        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
-            String linha;
-            while ((linha = br.readLine()) != null) {
-                String[] dados = linha.split(",");
-                if (dados.length < 1) throw new TarefaException("Erro no formato do arquivo.");
-
-                if (dados[0].equalsIgnoreCase(nomeUsuario)) {
-                    return true;
-                }
-            }
-        } catch (IOException e) {
-            throw new TarefaException("Erro ao ler o arquivo: " + e.getMessage());
-        }
-        return false;
     }
 }
