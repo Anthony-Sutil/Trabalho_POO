65eafd93768689ece52f10eca4a19b859a39dfb2
joao <joaovictorbalvedi@gmail.com>
2024-11-14 07:16:25 -0300

    ajustes finais interface

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,10 +1,16 @@
+import java.util.Date;
+
 public abstract class Tarefa {
     protected String titulo;
     protected String descricao;
+    protected int nivelImportancia;
+    protected Date dataFinalizacao;
 
-    public Tarefa(String titulo, String descricao) {
+    public Tarefa(String titulo, String descricao, int nivelImportancia, Date dataFinalizacao) {
         this.titulo = titulo;
         this.descricao = descricao;
+        this.nivelImportancia = nivelImportancia;
+        this.dataFinalizacao = dataFinalizacao;
     }
 
     public abstract void exibirDetalhes();
@@ -15,5 +21,13 @@ public abstract class Tarefa {
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public int getNivelImportancia() {
+        return nivelImportancia;
+    }
+
+    public Date getDataFinalizacao() {
+        return dataFinalizacao;
     }
 }
--- src/TarefaTrabalho.java
+++ src/TarefaTrabalho.java
@@ -1,10 +1,14 @@
+import java.util.Date;
+
 public class TarefaTrabalho extends Tarefa {
-    public TarefaTrabalho(String titulo, String descricao) {
-        super(titulo, descricao);
+    public TarefaTrabalho(String titulo, String descricao, int nivelImportancia, Date dataFinalizacao) {
+        super(titulo, descricao, nivelImportancia, dataFinalizacao);
     }
 
     @Override
     public void exibirDetalhes() {
-        System.out.println("Tarefa de Trabalho: " + titulo + " - " + descricao);
+        System.out.println("Tarefa de Trabalho: " + titulo + " - " + descricao +
+                ", Importância: " + nivelImportancia +
+                ", Data de Finalização: " + dataFinalizacao);
     }
 }
